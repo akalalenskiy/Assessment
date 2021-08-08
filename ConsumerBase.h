f47e671fac2826e5b2f294e94b7dfa11c8e55a3b
Andrey <akalalenskiy@gmail.com>
2021-08-08 23:07:55 +0300

    Doxygen comments added to ConsumerBase

--- ConsumerBase.h
+++ ConsumerBase.h
@@ -5,6 +5,14 @@
 namespace MultyQueue_NS
 {
 
+/// @brief Base class for consumers (subscribers)
+/// 
+/// @tparam TKey Queue key type
+/// 
+/// @tparam TValue Type of queuelement
+/// 
+/// @tparam TConsumer Type of consumer, derived from this class (CRTP pattern used)
+/// 
 template<typename TKey, typename TValue, typename TConsumer>
 class ConsumerBase
 {
